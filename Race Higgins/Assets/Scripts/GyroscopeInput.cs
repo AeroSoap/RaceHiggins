0f3bf17844f101e3a314dd8dd6d0d20da93459c3
TechEpic <signup1579@gmail.com>
2020-05-18 16:27:32 -0700

    Spinny cube

---
+++ Race Higgins/Assets/Scripts/GyroscopeInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeInput : MonoBehaviour {
+
+	Gyroscope gyro;
+
+	// Start is called before the first frame update
+	void Start() {
+		gyro = Input.gyro;
+		gyro.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.rotation = gyro.attitude;
+	}
+}
