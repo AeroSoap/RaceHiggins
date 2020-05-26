cfbb2769eed88a5d83b403e2efb570c8f1703be1
hjenningsramirez123 <h.jenningsramirez@digipen.edu>
2020-05-26 15:39:12 -0700

    Added a bare-bones menu and a script to change scenes

---
+++ Race Higgins/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,39 @@
+﻿// ChangeScenes.cs
+// By: Harry Jennings-Ramirez
+// Last Edited: May 26th, 2020
+// Description: Changes scenes, usually when a button is pushed
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    public void OpenGame() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void HelpScene() {
+        // For later if we decide to add it
+        // SceneManager.LoadScene("GameControls");
+    }
+
+    public void TitleScene() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitScene() {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
