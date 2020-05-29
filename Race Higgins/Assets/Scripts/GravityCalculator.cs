61d7ba2e332400fc33d41d0c7a49b184b51b5959
TechEpic <signup1579@gmail.com>
2020-05-29 13:08:14 -0700

    I don't think you understand the GRAVITY of the situation.

--- Race Higgins/Assets/Scripts/GravityCalculator.cs
+++ Race Higgins/Assets/Scripts/GravityCalculator.cs
@@ -1,10 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GravityCalculator : MonoBehaviour {
 
 	public static Vector3 GetGravity(Vector3 pos) {
+		string name = SceneManager.GetActiveScene().name;
+		if(name == "Level1") {
+			return Level1(pos);
+		}
+		if(name == "Level3") {
+			return Level3(pos);
+		}
+		return SampleGravity(pos);
+	}
+
+	static Vector3 Level1(Vector3 pos) {
+		return new Vector3(0, -50, 0);
+	}
+	static Vector3 Level3(Vector3 pos) {
+		return new Vector3(0, -50, 0);
+	}
+
+	static Vector3 SampleGravity(Vector3 pos) {
 		Vector3 rel = pos - new Vector3(395, 200, -226);
 		if(rel.magnitude < 155) {
 			return rel.normalized * 50;
