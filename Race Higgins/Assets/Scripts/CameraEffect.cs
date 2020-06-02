04edefc199be0bd94e66ab861d6a354a0eba88ad
TechEpic <signup1579@gmail.com>
2020-06-02 13:59:47 -0700

    haha go car zoom

---
+++ Race Higgins/Assets/Scripts/CameraEffect.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class CameraEffect : MonoBehaviour {
+
+	public float Smoothing;
+
+	Rigidbody rb;
+	Camera cam;
+
+	float fov;
+
+	// Start is called before the first frame update
+	void Start() {
+		rb = transform.parent.gameObject.GetComponent<Rigidbody>();
+		cam = GetComponent<Camera>();
+		fov = 60;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float target = rb.velocity.magnitude / 7 + 60;
+		fov = fov * Smoothing + target * (1 - Smoothing);
+		cam.fieldOfView = fov;
+		Debug.Log(rb.velocity.magnitude);
+	}
+}
