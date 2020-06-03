bbc37db5b39c53129043d4eefb1d1d1771ded0c1
TechEpic <signup1579@gmail.com>
2020-06-03 15:54:51 -0700

    haha time go tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock tick tock

--- Race Higgins/Assets/Scripts/ScoreText.cs
+++ Race Higgins/Assets/Scripts/ScoreText.cs
@@ -8,12 +8,16 @@ public class ScoreText : MonoBehaviour {
 	void Start() {
 		// Get the text component
 		Text text = GetComponent<Text>();
+		text.text += toTime(Manager.time);
+	}
+
+	public static string toTime(float time) {
 		// Calculate the minutes, seconds, and milliseconds based on the decimal seconds
-		int mins = (int)(Manager.time / 60);
-		int secs = (int)(Manager.time) % 60;
-		int ms = (int)(Manager.time * 1000 + 0.5) % 1000;
+		int mins = (int)(time / 60);
+		int secs = (int)(time) % 60;
+		int ms = (int)(time * 1000 + 0.5) % 1000;
 		// Add the formatted string representing the time to the UI
-		text.text += mins.ToString().PadLeft(2, '0')
-			+ ":" + secs.ToString().PadLeft(2, '0') + ":" + ms.ToString().PadLeft(3, '0');
+		return mins.ToString().PadLeft(2, '0')	+ ":" + secs.ToString().PadLeft(2, '0')
+			+ ":" + ms.ToString().PadLeft(3, '0');
 	}
 }
