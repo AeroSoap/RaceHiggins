fbcfaa07d3d02c2deadd1028a2bb8c7594556dde
bluephaser <liam.binford@digipen.edu>
2020-06-04 14:54:24 -0700

    sergv

---
+++ Race Higgins/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,28 @@
+﻿/*
+ ExitGame.cs
+ By: Liam Binford
+ Date: 6/4/20
+ Description: Closes the game when esc is pressed
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player presses escape, quit the game
+        if(Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
